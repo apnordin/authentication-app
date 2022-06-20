3e593d467610d0b3b2750a2254fafd3f60cf5eaf
Alex Nordin <apnordin@gmail.com>
2022-06-20 19:02:28 -0400

    Initialized project, frontend styling, created FE signup

---
+++ src/components/AuthPage.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+// import "../App.css";
+
+interface AuthPageProps {
+  isSignup: boolean;
+  setIsSignup: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AuthPage: React.FC<AuthPageProps> = ({isSignup, setIsSignup}) => {
+  const SwapButton = () => {
+    return (
+      <div className="swap-container">
+        {isSignup ? "Already have an account?" : "Don't have an account?"}
+        <button onClick={() => setIsSignup(!isSignup)} className="swap-button">
+          {isSignup ? "Log In" : "Sign Up"}
+        </button>
+      </div>
+    );
+  };
+
+  const SignupPage = () => {
+    const [firstName, setFirstName] = React.useState("");
+    const [lastName, setLastName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const handleSubmit = (e: {preventDefault: () => void}) => {
+      e.preventDefault();
+      console.log(firstName, lastName, email, password);
+    };
+
+    return (
+      <>
+        <h1>Create your account</h1>
+        <form className="signup-form" onSubmit={handleSubmit}>
+          <input
+            placeholder="First Name"
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            placeholder="Last Name"
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <input
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="sign-up">Sign up</button>
+        </form>
+      </>
+    );
+  };
+
+  const LoginPage = () => {
+    const placeholdervariable: string = "hello";
+    return (
+      <>
+        <h1>Log in to your account</h1>
+      </>
+    );
+  };
+
+  return (
+    <div className="auth-container">
+      {isSignup ? <SignupPage /> : <LoginPage />} <SwapButton />
+    </div>
+  );
+};
+
+export default AuthPage;
